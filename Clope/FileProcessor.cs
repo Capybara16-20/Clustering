7be8741434c13e15d39c0ad31500e6931d2d1902
Dmitry Sharonov <capybara20.07@mail.ru>
2022-02-20 01:32:15 +0300

    Add transactions generation and file handling class

---
+++ Clope/FileProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Clope
+{
+    static class FileProcessor
+    {
+        const string fileNotFoundErrorMessage = "Файл не найден.";
+
+        public static string[][] ReadTransactions(string fileName)
+        {
+            const char elementSeparator = '\t';
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            if (filePath == null)
+                throw new ArgumentNullException(nameof(filePath));
+
+            string[] lines = ReadLines(filePath);
+
+            string[][] transactions = null;
+
+            throw new NotImplementedException();
+
+            return transactions;
+        }
+
+        private static string[] ReadLines(string filePath)
+        {
+            string[] lines;
+            if (File.Exists(filePath))
+                lines = File.ReadAllLines(filePath);
+            else
+                throw new FileNotFoundException(fileNotFoundErrorMessage);
+
+            return lines;
+        }
+
+        public static void WriteToFile(string fileName, string line)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            if (filePath == null)
+                throw new ArgumentNullException(nameof(filePath));
+
+            FileStream file = new(filePath, FileMode.Create);
+            using (StreamWriter sw = new(file))
+            {
+                sw.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Clope/TransactionGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Clope
+{
+    static class TransactionGenerator
+    {
+        public static void GenerateTransactions(int count, int minLength, int maxLength, string fileName)
+        {
+            const int startSymbolCode = 97;
+            const int endSymbolCode = 122;
+            const string symbolPattern = "{0}\t";
+
+            Random rand = new();
+
+            StringBuilder set = new();
+            for (int i = 0; i < count; i++)
+            {
+                StringBuilder transaction = new();
+                for (int j = 0; j < rand.Next(minLength, maxLength + 1); j++)
+                {
+                    transaction.Append(string.Format(symbolPattern, (char)rand.Next(startSymbolCode, endSymbolCode + 1)));
+                }
+
+                set.Append(transaction);
+                set.AppendLine();
+            }
+
+            FileProcessor.WriteToFile(fileName, set.ToString());
+        }
+    }
+}
