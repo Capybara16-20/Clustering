22292f6f6857be3776921f9ba0d0e415e4752add
Dmitry Sharonov <capybara20.07@mail.ru>
2022-02-20 01:34:14 +0300

    Add system files and ClopeProccessor class

---
+++ Clope/ClopeProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Clope
+{
+    public class ClopeProcessor
+    {
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Конструктор, вызывающий метод генерации набора транзакций
+        /// </summary>
+        /// <param name="count">Количество транзакций в наборе</param>
+        /// <param name="minLength">Минимальная длина транзакции</param>
+        /// <param name="maxLength">Максимальная длина транзакции</param>
+        /// <param name="fileName">Имя файла</param>
+        public ClopeProcessor(int count, int minLength, int maxLength, string fileName)
+        {
+            TransactionGenerator.GenerateTransactions(count, minLength, maxLength, fileName);
+            FileName = fileName;
+        }
+
+        /// <summary>
+        /// Конструктор для работы с имеющимся набором данных
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        public ClopeProcessor(string fileName)
+        {
+            FileName = fileName;
+        }
+
+
+    }
+}
