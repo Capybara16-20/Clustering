f94fe61aaac4d3dda4134c18c8228cad5a9035ab
Dmitry Sharonov <capybara20.07@mail.ru>
2022-02-21 05:10:22 +0300

    Add description

--- Clope/TransactionGenerator.cs
+++ Clope/TransactionGenerator.cs
@@ -3,8 +3,18 @@ using System.Text;
 
 namespace Clope
 {
+    /// <summary>
+    /// Класс для генерации тестовых наборов данных
+    /// </summary>
     static class TransactionGenerator
     {
+        /// <summary>
+        /// Метод генерации тестового набора данных
+        /// </summary>
+        /// <param name="count">Количество транзакций</param>
+        /// <param name="minLength">Минимальная длина транзакции</param>
+        /// <param name="maxLength">Максимальная длина транзакции</param>
+        /// <param name="fileName">Имя создаваемого файла</param>
         public static void GenerateTransactions(int count, int minLength, int maxLength, string fileName)
         {
             const int startSymbolCode = 97;
